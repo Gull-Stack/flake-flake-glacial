4f09157ef2bd919d5f28f7478e2f192a0089e109
Bryce Morgan <brycedmorgan@gmail.com>
2026-03-23 11:30:15 -0600

    Bot Bogey: Full AEO schema layer — Organization, LegalService, FAQPage, BreadcrumbList, Attorney, Service schemas + robots.txt

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/.well-known");
   eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addPassthroughCopy("src/robots.txt");
 
   // Watch CSS files for changes
   eleventyConfig.addWatchTarget("./src/css/");
